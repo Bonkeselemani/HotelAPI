3a33e13295f4ffc96a19135d9c22664447af9e43
Bonkeselemani <bselemani@premierss.com>
2022-10-21 22:10:11 -0500

    Initial commit

---
+++ src/test/java/io/Bonke/HotelAPI/HotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.Bonke.HotelAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
